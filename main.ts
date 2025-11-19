aef4c878ba8dfed7b03963a3c9d598b62fe01468
default <zoe.wrubleski@stu.ocsb.ca>
2025-11-19 14:26:29 +0000

    happy birthday with user name

---
+++ main.ts
@@ -0,0 +1,20 @@
+/**
+ * @author Zoe Wrubleski
+ * @version 1.0.0
+ * @date 2025-11-19
+ * @fileoverview This program displays happy birthday with the users name
+ */
+
+// set variable
+let userName: string;
+
+// prompt user name
+userName = prompt("What is your name?");
+
+// display happy birthday
+console.log("Happy birthday to you!");
+console.log("Happy birthday to you!");
+console.log("Happy birthday dear, " + userName + "!");
+console.log("Happy birthday to you!");
+
+console.log("\nDone.");
